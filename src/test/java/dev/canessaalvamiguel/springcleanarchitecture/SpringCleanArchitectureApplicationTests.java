eacd3d88ee67d0ce4aa0592a028cea1cf40923a8
Miguel Canessa <canessaalvamiguel@gmail.com>
2024-04-04 20:34:51 -0500

    Initial commit

---
+++ src/test/java/dev/canessaalvamiguel/springcleanarchitecture/SpringCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.canessaalvamiguel.springcleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCleanArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
